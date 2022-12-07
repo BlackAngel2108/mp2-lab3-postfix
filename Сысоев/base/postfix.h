058229de1d553a45ff2a9caf76b31b1f5cff7b53
BlackAngel2108 <luba.sobol.nn@mail.ru>
2022-12-07 19:43:56 +0300

    first commit

--- Сысоев/base/postfix.h
+++ Сысоев/base/postfix.h
@@ -1,24 +1,54 @@
-﻿#ifndef __POSTFIX_H__
-#define __POSTFIX_H__
-
+﻿#include<iostream>
 #include <string>
-#include "stack.h"
-
+//#include "stack.h"
+#include <vector>
+#include <map>
+#include <stack>
 using namespace std;
 
 class TPostfix
 {
-  string infix;
-  string postfix;
-public:
-  TPostfix()
-  {
-    infix = "a + b";
-  }
-  string GetInfix() { return infix; }
-  string GetPostfix() { return postfix; }
-  string ToPostfix();
-  double Calculate(); // Ввод переменных, вычисление по постфиксной форме
-};
+    string input_expression;
+    enum lexemType {
+        nothing,//начало выражения
+        number, // 4; -4; .4; 
+        variable, //переменная____ a;my_argument - right_____-a;a1 - wrong!
+        operation, //+, -, *, /
+        operator_open, // (
+        operator_close // )
+    };
+    vector<pair<lexemType, string>> infix; //набор лексем со значениями
+    vector<pair<lexemType, string>> postfix;
+    map<string, int> priority;
+    map<string, double> operands; //набор переменных(variable) со значениями (по умолчанию в них нули)
 
-#endif
+    bool isNumber(char c);      // 0 ... 9
+    bool isLetter(char c); // a ... z, A ... Z, _
+    bool isOperation(char c);   // +, -, *, /
+    bool isMinus(char c);       // -
+    bool isPoint(char c);       // .
+    bool isOperator_open(char c);       // (
+    bool isOperator_close(char c);         // )
+
+    void Parse(); //разложить на лексемы
+    void ToPostfix();//сделать опбратную польскую запись для подсчета
+    void GetValues(istream& input, ostream& output);
+public:
+
+    TPostfix(string _input_expression) :input_expression(_input_expression) {
+        this->priority = { {" + ",1},{" - ",1},{" * ",2},{" / ",2} };
+        ToPostfix();
+    }
+    string GetInfix() { return input_expression; }
+
+    string GetPostfix() {
+        string ans;
+        for (auto i : postfix) {
+            ans += ' ';
+            ans += i.second;
+        }
+        return ans;
+    }
+
+    double Calculate(istream& input, ostream& output);// Ввод переменных, вычисление по постфиксной форме
+};
